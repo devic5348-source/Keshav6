a3233d46fc0884882be8f44b68fd5013ab71310f
devic5348-source <devic5348@gmail.com>
2025-08-19 21:36:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'publicfilechannel1') #
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/NexusSupportGroup')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movies_request_groups_9')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/nexuskingdom6')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'NexusSupportGroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/KeshavTheOwner')
@@ -196,5 +196,6 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
